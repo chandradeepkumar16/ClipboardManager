7a4224c650eaa8846b2cb62b2c4809499aef724b
chandradeepkumar16 <chandradeepkumar2002.ck@gmail.com>
2023-10-12 13:44:27 +0530

    edit screen modified

--- app/src/main/java/com/example/clipboardmanager/userInterface/editScreen/EditScreen.kt
+++ app/src/main/java/com/example/clipboardmanager/userInterface/editScreen/EditScreen.kt
@@ -2,17 +2,16 @@ package com.example.clipboardmanager.userInterface.editScreen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -21,47 +20,49 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
-
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditScreen(
-    navController: NavController,
-//    initialText: (String) -> Unit,
-//    onSave: (String) -> Unit,
-//    onNavigateUp: () -> Unit
+    navController: NavController
 ) {
+    var editedText by remember { mutableStateOf("new text here") }
 
-//    var editedText by remember { mutableStateOf(initialText) }
-    var editedText="new text here"
-    
-
-    Scaffold(topBar = {
-        Row(horizontalArrangement = Arrangement.Start ) {
-            Icon(imageVector = Icons.Default.ArrowBack,
-                contentDescription ="Arrow Back",
-                modifier = Modifier.clickable {
-                    navController.popBackStack()
-                })
-            Spacer(modifier = Modifier.width(10.dp))
-            Text(text = "back")
-
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Back") },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Arrow Back",
+                        modifier = Modifier.clickable {
+                            navController.popBackStack()
+                        }
+                    )
+                }
+            )
         }
-    }) { }
-
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
+                .padding(16.dp).verticalScroll(rememberScrollState())
+            ,
         ) {
+            Spacer(modifier = Modifier.height(60.dp))
             TextField(
                 value = editedText,
                 onValueChange = { newText ->
@@ -72,28 +73,33 @@ fun EditScreen(
                 ),
                 keyboardActions = KeyboardActions(
                     onDone = {
-//                    onSave(editedText)
-//                    onNavigateUp()
+                        // Handle the save action here
                     }
                 ),
-                textStyle = TextStyle.Default, // You can use the appropriate text style
+                textStyle = TextStyle.Default,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp)
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(25.dp))
 
             Button(
                 onClick = {
-//                onSave(editedText)
-//                onNavigateUp()
                 },
-                modifier = Modifier.align(Alignment.End)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+
             ) {
                 Text(text = "Save")
             }
         }
+    }
 }
+
+
+
+
 
 
